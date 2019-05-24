672c4a8483fc914006284714941f0a60dd2a00b1
Arvind Purohith <purohith.arvind@gmail.com>
2019-05-24 16:44:23 +0530

    changed IAM role and policy

--- MySql-Template/modules/iam/iam.tf
+++ MySql-Template/modules/iam/iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ec2_s3_access_role" {
-  name               = "gam-s3-role1"
+  name               = "gam-s3-role"
   assume_role_policy = "${file("modules/iam/assumerolepolicy.json")}"
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "gam-test-policy1"
+  name        = "gam-test-policy"
   description = "A test policy"
   policy      = "${file("modules/iam/policys3bucket.json")}"
 }
 
 resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "gam-test-attachment1"
+  name       = "gam-test-attachment"
   roles      = ["${aws_iam_role.ec2_s3_access_role.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
 
 resource "aws_iam_instance_profile" "test_profile" {
-  name  = "gam_test_profile1"
+  name  = "gam_test_profile"
   roles = ["${aws_iam_role.ec2_s3_access_role.name}"]
 }
 
