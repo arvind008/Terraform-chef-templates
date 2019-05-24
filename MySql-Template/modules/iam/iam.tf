4a81739d93ac83b5eb487cff708383203cfe4726
Arvind Purohith <purohith.arvind@gmail.com>
2019-05-24 17:06:44 +0530

    Dynamic name for role and policy AWS

--- MySql-Template/modules/iam/iam.tf
+++ MySql-Template/modules/iam/iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ec2_s3_access_role" {
-  name               = "s3-role"-${var.stackname}
+  name               = "s3-role-${var.stackname}"
   assume_role_policy = "${file("modules/iam/assumerolepolicy.json")}"
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "policy"-${var.stackname}
+  name        = "policy-${var.stackname}"
   description = "A test policy"
   policy      = "${file("modules/iam/policys3bucket.json")}"
 }
 
 resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "attachment"-${var.stackname}
+  name       = "attachment-${var.stackname}"
   roles      = ["${aws_iam_role.ec2_s3_access_role.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
 
 resource "aws_iam_instance_profile" "test_profile" {
-  name  = "profile"-${var.stackname}
+  name  = "profile-${var.stackname}"
   roles = ["${aws_iam_role.ec2_s3_access_role.name}"]
 }
 
